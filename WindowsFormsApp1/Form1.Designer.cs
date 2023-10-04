50fe50338952671b45b84300f8898d5171ab40bc
kaaateeeriiinaaa <koloskova70402@mail.ru>
2023-10-04 21:17:55 +0300

    Добавлена форма с таблицей

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.библиотекаDataSet = new WindowsFormsApp1.БиблиотекаDataSet();
+            this.авторBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.авторTableAdapter = new WindowsFormsApp1.БиблиотекаDataSetTableAdapters.АвторTableAdapter();
+            this.iDавтораDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.фамилияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.имяDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.отчествоDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.библиотекаDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.авторBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDавтораDataGridViewTextBoxColumn,
+            this.фамилияDataGridViewTextBoxColumn,
+            this.имяDataGridViewTextBoxColumn,
+            this.отчествоDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.авторBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(818, 481);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // библиотекаDataSet
+            // 
+            this.библиотекаDataSet.DataSetName = "БиблиотекаDataSet";
+            this.библиотекаDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // авторBindingSource
+            // 
+            this.авторBindingSource.DataMember = "Автор";
+            this.авторBindingSource.DataSource = this.библиотекаDataSet;
+            // 
+            // авторTableAdapter
+            // 
+            this.авторTableAdapter.ClearBeforeFill = true;
+            // 
+            // iDавтораDataGridViewTextBoxColumn
+            // 
+            this.iDавтораDataGridViewTextBoxColumn.DataPropertyName = "ID_автора";
+            this.iDавтораDataGridViewTextBoxColumn.HeaderText = "ID_автора";
+            this.iDавтораDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.iDавтораDataGridViewTextBoxColumn.Name = "iDавтораDataGridViewTextBoxColumn";
+            this.iDавтораDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // фамилияDataGridViewTextBoxColumn
+            // 
+            this.фамилияDataGridViewTextBoxColumn.DataPropertyName = "Фамилия";
+            this.фамилияDataGridViewTextBoxColumn.HeaderText = "Фамилия";
+            this.фамилияDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.фамилияDataGridViewTextBoxColumn.Name = "фамилияDataGridViewTextBoxColumn";
+            this.фамилияDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // имяDataGridViewTextBoxColumn
+            // 
+            this.имяDataGridViewTextBoxColumn.DataPropertyName = "Имя";
+            this.имяDataGridViewTextBoxColumn.HeaderText = "Имя";
+            this.имяDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.имяDataGridViewTextBoxColumn.Name = "имяDataGridViewTextBoxColumn";
+            this.имяDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // отчествоDataGridViewTextBoxColumn
+            // 
+            this.отчествоDataGridViewTextBoxColumn.DataPropertyName = "Отчество";
+            this.отчествоDataGridViewTextBoxColumn.HeaderText = "Отчество";
+            this.отчествоDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.отчествоDataGridViewTextBoxColumn.Name = "отчествоDataGridViewTextBoxColumn";
+            this.отчествоDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 553);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.библиотекаDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.авторBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private БиблиотекаDataSet библиотекаDataSet;
+        private System.Windows.Forms.BindingSource авторBindingSource;
+        private БиблиотекаDataSetTableAdapters.АвторTableAdapter авторTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDавтораDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn фамилияDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn имяDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn отчествоDataGridViewTextBoxColumn;
+    }
+}
+
